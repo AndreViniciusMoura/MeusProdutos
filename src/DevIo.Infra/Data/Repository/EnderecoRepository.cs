08411be30f327d0c2e86201536be13e071fc3493
André Moura <amoura@softworks.com>
2022-11-01 08:58:55 +0000

    Finalizando o padrao Repository.

---
+++ src/DevIo.Infra/Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Business.Core.Data;
+using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace DevIo.Infra.Data.Repository
+{
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    {
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId) =>
+            await ObterPorId(fornecedorId);
+    }
+}
---
+++ src/DevIo.Infra/Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,29 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DevIo.Infra.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id) =>
+            await Db.Fornecedores.AsNoTracking()
+            .Include(f => f.Endereco)
+            .FirstOrDefaultAsync(f => f.Id == id);
+
+        public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id) =>
+            await Db.Fornecedores.AsNoTracking()
+            .Include(f => f.Endereco)
+            .Include(f => f.Produtos)
+            .FirstOrDefaultAsync(f => f.Id == id);
+
+        public override async Task Remover(Guid id)
+        {
+            var fornecedor = await ObterPorId(id);
+            fornecedor.Ativo = false;
+
+            await Atualizar(fornecedor);
+        }
+    }
+}
