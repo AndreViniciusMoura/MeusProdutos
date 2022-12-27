3af741ae70469c14ea63196e212fecdcdbef7659
André Moura <amoura@softworks.com>
2022-12-27 17:27:58 +0000

    Incluindo a atualizacao do endereco.

--- src/DevIO.AppMvc/Controllers/FornecedoresController.cs
+++ src/DevIO.AppMvc/Controllers/FornecedoresController.cs
@@ -111,6 +111,45 @@ namespace DevIO.AppMvc.Controllers
             return RedirectToAction("Index");
         }
 
+        [Route("obter-endereco-fornecedor/{id:guid}")]
+        public async Task<ActionResult> ObterEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor is null) return HttpNotFound();
+
+            return PartialView("_DetalheEndereco", fornecedor);
+        }
+
+        [Route("atualizar-endereco-fornecedor/{id:guid}")]
+        [HttpGet]
+        public async Task<ActionResult> AtualizarEndereco(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor is null) return HttpNotFound();
+
+            return PartialView("_AtualizarEndereco", new FornecedorViewModel { Endereco = fornecedor.Endereco });
+        }
+
+        [Route("atualizar-endereco-fornecedor/{id:guid}")]
+        [HttpPost]
+        public async Task<ActionResult> AtualizarEndereco(FornecedorViewModel fornecedorViewModel)
+        {
+            ModelState.Remove("Nome");
+            ModelState.Remove("Documento");
+
+            if (!ModelState.IsValid) return PartialView("_atualizarEndereco", fornecedorViewModel);
+
+            await _fornecedorService.AtualizarEndereco(_mapper.Map<Endereco>(fornecedorViewModel.Endereco));
+
+            //TODO:
+            // E se nao der certo?
+
+            var url = Url.Action("ObterEndereco", "Fornecedores", new { id = fornecedorViewModel.Endereco.FornecedorId });
+            return Json(new { success = true, url });
+        }
+
         private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id) =>
             _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorEndereco(id));
 
--- src/DevIO.AppMvc/Scripts/site.js
+++ src/DevIO.AppMvc/Scripts/site.js
@@ -105,3 +105,46 @@ function bindForm(dialog) {
         return false;
     });
 }
+
+function SetModal() {
+
+    $(document).ready(function () {
+        $(function () {
+            $.ajaxSetup({ cache: false });
+
+            $("a[data-modal]").on("click", function (e) {
+                $("#myModalContent").load(this.href,
+                    function () {
+                        $("#myModal").modal({
+                            keyboard: true
+                        },
+                            "show");
+                        bindForm(this);
+                    });
+                return false;
+            })
+        })
+    });
+}
+
+function bindform(dialog) {
+    $('form', dialog).submit(function () {
+        $.ajax({
+            url: this.action,
+            type: this.method,
+            data: $(this).serialize(),
+            success: function (result) {
+                if (result.success) {
+                    $('#myModal').modal('hide');
+                    $('#EnderecoTarget').load(result.url); // Carrega o resultado HTML para a div demarcada
+                } else {
+                    $('#myModalContent').html(result);
+                    bindform(dialog);
+                }
+            }
+        });
+
+        SetModal();
+        return false;
+    });
+}
